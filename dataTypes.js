22836d4e73313298e9cb257b75f93e7a64ec311c
Sparsh Kapoor <162795614+SparshKapoor-CODER@users.noreply.github.com>
2025-01-01 21:26:30 +0530

    Create dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treats all JS code as newer version of JS
+//alert(3 + 3); // error as we are using js not a browser
+console.log(3 + 3) // 6
+console.log(6+9); console.log(9-2); // this will run but tihis is NOT A GOOD PRACTICE!!!
+
+//tc.39 and mdn.io.... are the good resources for JS
+//JS is case sensitive
+
+let myName = "Sparsh";
+let myAge = 18;
+let myCity = "Agra";
+let isLoggedIn = true;
+
+// bigint is simmmiler to long in java
+// null is a special value that represents "nothing", "empty" or "value unknown"
+// undefined is a special value that represents "not assigned" or "unknown value"
+// null and undefined are two different things
+// the difference between null and undefined is that null is a value that represents "nothing", "empty" or "value unknown" while undefined is a value that represents "not assigned" or "unknown value"
+// symbol is a primitive data type that is used to generate unique identifiers for objects.
+
+
+// object is a non-primitive data type that represents a collection of key-value pairs. It is used to store complex data structures and can be used to represent real-world entities.
+console.log(typeof myName); // string
+console.log(typeof myAge); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
