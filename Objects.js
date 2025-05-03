d28ede239ae36dfc44e3de998229d0f6cbc6123d
Sparsh Kapoor <162795614+SparshKapoor-CODER@users.noreply.github.com>
2025-05-03 13:20:09 +0530

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -50,3 +50,35 @@ VTOP.Attendice = 96;
 
 
 console.log(VTOP);
+
+
+const obj1 = {
+    1: "a",
+    2: "b"
+};
+
+const obj2 = {
+    3: "c",
+    4: "d"
+};
+
+const obj3 = { obj1, obj2};   // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }      object inside object
+
+console.log(obj3);
+
+// Now how to tackel this situation?
+
+const obj4 = Object.assign({},obj1,obj2);  // { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+console.log(obj4);
+
+// Documentation   ====>   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+
+
+//   We will not use the above mostly the thing we will use is the spread operator. The same that we used in array (...)
+
+const obj5 = {...obj1 , ...obj2};
+console.log(obj5);
+
+
+
+// find more on browser console
